e9cb9f74b6ca89051f0cecb4629d3f9243147e62
Louis Lagrange <lagrange.louis@gmail.com>
2017-04-20 18:30:19 +0200

    Add push button subscription button

--- src/app.js
+++ src/app.js
@@ -1,6 +1,22 @@
 document.addEventListener("DOMContentLoaded", () => {
+    let isPushEnabled = false;
+
+    const pushButton = document.querySelector('#push-subscription-button');
+    if (!pushButton) {
+        return;
+    }
+
+    pushButton.addEventListener('click', function() {
+        if (isPushEnabled) {
+            push_unsubscribe();
+        } else {
+            push_subscribe();
+        }
+    });
+
     if (!('serviceWorker' in navigator)) {
         console.warn("Service workers aren't supported on this browser");
+        changePushButtonState('incompatible');
         return;
     }
 
@@ -9,5 +25,40 @@ document.addEventListener("DOMContentLoaded", () => {
         console.log('[SW] Service worker has been registered');
     }, e => {
         console.error('[SW] Service worker registration failed', e);
+        changePushButtonState('incompatible');
     });
+
+    function changePushButtonState (state) {
+        switch (state) {
+            case 'enabled':
+                pushButton.disabled = false;
+                pushButton.title = "Push notifications enabled";
+                isPushEnabled = true;
+                break;
+            case 'disabled':
+                pushButton.disabled = false;
+                pushButton.title = "Push notifications disabled";
+                isPushEnabled = false;
+                break;
+            case 'computing':
+                pushButton.disabled = true;
+                pushButton.title = "Loading...";
+                break;
+            case 'incompatible':
+                pushButton.disabled = true;
+                pushButton.title = "Push notifications are not compatible with this browser";
+                break;
+            default:
+                console.error('Unhandled push button state', state);
+                break;
+        }
+    }
+
+    function push_subscribe() {
+        changePushButtonState('computing');
+    }
+
+    function push_unsubscribe() {
+        changePushButtonState('computing');
+    }
 });
