55f56d362cdf6c51be8dc654f93dce37a9db28d1
Louis Lagrange <lagrange.louis@gmail.com>
2017-04-21 22:23:47 +0200

    Add service worker

---
+++ src/serviceWorker.js
@@ -0,0 +1,19 @@
+self.addEventListener('push', function (event) {
+    if (!(self.Notification && self.Notification.permission === 'granted')) {
+        return;
+    }
+
+    const sendNotification = body => {
+        // you could refresh a notification badge here with postMessage API
+        const title = "Web Push example";
+
+        return self.registration.showNotification(title, {
+            body,
+        });
+    };
+
+    if (event.data) {
+        const message = event.data.text();
+        event.waitUntil(sendNotification(message));
+    }
+});
