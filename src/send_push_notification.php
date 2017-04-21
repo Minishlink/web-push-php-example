421e24b736415f2be13d1f2ba919272ef8d5e39e
Louis Lagrange <lagrange.louis@gmail.com>
2017-04-21 22:13:36 +0200

    Add endpoint to send notification

---
+++ src/send_push_notification.php
@@ -0,0 +1,26 @@
+<?php
+require __DIR__ . '/../vendor/autoload.php';
+use Minishlink\WebPush\WebPush;
+
+// here I'll get the subscription endpoint in the POST parameters
+// but in reality, you'll get this information in your database
+// because you already stored it (cf. push_subscription.php)
+$subscription = json_decode(file_get_contents('php://input'), true);
+
+$auth = array(
+    'VAPID' => array(
+        'subject' => 'https://github.com/Minishlink/web-push-php-example/',
+        'publicKey' => 'BCmti7ScwxxVAlB7WAyxoOXtV7J8vVCXwEDIFXjKvD-ma-yJx_eHJLdADyyzzTKRGb395bSAtxlh4wuDycO3Ih4',
+        'privateKey' => 'HJweeF64L35gw5YLECa-K7hwp3LLfcKtpdRNK8C_fPQ', // in the real world, this would be in a secret file
+    ),
+);
+
+$webPush = new WebPush($auth);
+
+$res = $webPush->sendNotification(
+    $subscription['endpoint'],
+    "Hello!",
+    $subscription['key'],
+    $subscription['token'],
+    true
+);
