b2de6e8bef899cf40b7fd7ad5969c81c79ef00b2
Code is Talk <35674135+codeistalk@users.noreply.github.com>
2018-04-03 00:18:07 +0700

    Update router.php (#13)
    * Update router.php
    
    added strict file check
    double quote to single quote.
    
    * Update router.php
    
    * Update router.php

--- router.php
+++ router.php
@@ -5,7 +5,7 @@
 // DON'T USE IN PRODUCTION, please code your own router or use a framework!
 
 chdir(__DIR__);
-$filePath = realpath('./src/'.ltrim($_SERVER["REQUEST_URI"], '/'));
+$filePath = realpath('./src/'.ltrim($_SERVER['REQUEST_URI'], '/'));
 if ($filePath && is_dir($filePath)){
     // attempt to find an index file
     foreach (['index.php', 'index.html'] as $indexFile) {
@@ -18,23 +18,23 @@ if ($filePath && is_dir($filePath)){
 if ($filePath && is_file($filePath)) {
     // 1. check that file is not outside of this directory for security
     // 2. check for circular reference to router.php
-    // 3. don't serve dotfiles
+    // 3. don't serve dot files
     if (strpos($filePath, __DIR__ . DIRECTORY_SEPARATOR) === 0 &&
-        $filePath != __DIR__ . DIRECTORY_SEPARATOR . 'router.php' &&
-        substr(basename($filePath), 0, 1) != '.'
+        $filePath !== __DIR__ . DIRECTORY_SEPARATOR . 'router.php' &&
+        substr(basename($filePath), 0, 1) !== '.'
     ) {
-        if (strtolower(substr($filePath, -4)) == '.php') {
+        if (strtolower(substr($filePath, -4)) === '.php') {
             include $filePath;
         } else {
-            if (strtolower(substr($filePath, -3)) == '.js') {
-                Header('Content-Type: text/javascript');
+            if (strtolower(substr($filePath, -3)) === '.js') {
+                header('Content-Type: text/javascript');
             }
             readfile ($filePath);
         }
     } else {
         // disallowed file
-        header("HTTP/1.1 404 Not Found");
-        echo "404 Not Found";
+        header('HTTP/1.1 404 Not Found');
+        echo '404 Not Found';
     }
 } else {
     // rewrite to our index file
